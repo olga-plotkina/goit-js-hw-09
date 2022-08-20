0b2d4894965b88fc0ee00df5e191c6a80c46ab7e
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-20 22:06:36 +0100

    add colorpicker

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,14 @@
-
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onButtonChangeColorOfBody = e => {
+  document.body.style.backgroundColor = getRandomHexColor;
+};
+
+refs.startBtn.addEventListener('click', onButtonChangeColorOfBody);
