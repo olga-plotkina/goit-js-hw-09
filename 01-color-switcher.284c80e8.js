1c4cef75fb5b8085c6ef9f105f1e203d9c756ae1
olga-plotkina <101660609+olga-plotkina@users.noreply.github.com>
2022-08-20 21:47:02 +0000

    Deploying to gh-pages from @ olga-plotkina/goit-js-hw-09@8b2ec726d42543ee7afb98618c91b06dcbc7184f 🚀

--- 01-color-switcher.284c80e8.js
+++
@@ -1,2 +0,0 @@
-!function(){function t(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}({startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")}).startBtn.addEventListener("click",(function(n){document.body.style.backgroundColor=t}))}();
-//# sourceMappingURL=01-color-switcher.284c80e8.js.map
