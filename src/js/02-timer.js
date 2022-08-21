46d889e45a9129ac2005c82bcdc1403d9c360053
olga-plotkina <olga.semenyuta@gmail.com>
2022-08-21 15:14:38 +0100

    add timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,7 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const startBtn = document.querySelector('button[data-start]');
 startBtn.setAttribute('disabled', '');
+let selectedDate = 0;
 
 const options = {
   enableTime: true,
@@ -10,12 +11,38 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] <= new Date()) {
+    if (selectedDates[0] <= Date.now()) {
       alert('Please choose a date in the future');
     } else {
       startBtn.removeAttribute('disabled');
+      selectedDate = selectedDates[0];
     }
   },
 };
 
 flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const timer = {
+  start() {
+    setInterval(() => {
+      const currenTime = Date.now;
+      const deltaTime = selectedDate - currenTime;
+      const timeComponents = convertMs(deltaTime);
+      console.log(timeComponents);
+    }, 1000);
+  },
+};
